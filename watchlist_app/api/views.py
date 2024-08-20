3be08c2fdff4f5e8f7b5845c3f56ecb5ac262698
Sanju Naskar <sanjunaskar102@gmail.com>
2024-08-20 21:13:51 +0530

    Status code edited

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,6 +3,7 @@ from watchlist_app.models import Movie
 from watchlist_app.api.serializiers import MovieSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -25,12 +26,18 @@ def movie_list(request):
 @api_view(['GET','PUT','DELETE'])
 def movie_deatails(request,pk):
     if request.method=="GET":
-        movie=Movie.objects.get(pk=pk)
+        try:
+            movie=Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'message':'Movie not found !'}, status=status.HTTP_404_NOT_FOUND)
         serializer=MovieSerializer(movie)
         return Response(serializer.data)
     
     elif request.method=="PUT":
-        movie=Movie.objects.get(pk=pk)
+        try:
+            movie=Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Message':'Movie matching query does not exist'},status=status.HTTP_400_BAD_REQUEST)
         serializer=MovieSerializer(movie,data=request.data)
         if serializer.is_valid():
             serializer.save()
