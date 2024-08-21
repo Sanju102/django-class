f79fba49ffdb093b4210411fd2fb954a95c911d2
Sanju Naskar <sanjunaskar102@gmail.com>
2024-08-21 10:16:02 +0530

    Validation added

--- watchlist_app/api/serializiers.py
+++ watchlist_app/api/serializiers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def not_less_than_two(value):
+    if len(value) < 2:
+        raise serializers.ValidationError('Name is too short !')
+    else:
+        return value
+
 class MovieSerializer(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField()
+    name=serializers.CharField(validators=[not_less_than_two])
     description=serializers.CharField()
     active=serializers.BooleanField()
 
@@ -16,3 +22,10 @@ class MovieSerializer(serializers.Serializer):
         instance.active=validated_data.get('active',instance.active)
         instance.save()
         return instance
+
+    #  Object-level validation   
+    def validate(self, data):
+        if data["name"]==data["description"]:
+            raise serializers.ValidationError('Name and description cannot be same !')
+        else:
+            return data
