f9b7ee79f57efb2f10677c077d82a88f0d240303
Sanju Naskar <sanjunaskar102@gmail.com>
2024-08-23 21:08:07 +0530

    Custome  field added

--- watchlist_app/api/serializiers.py
+++ watchlist_app/api/serializiers.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    len_of_name=serializers.SerializerMethodField() #mention the custome field
+
+
     class Meta:
         model=Movie
         fields="__all__"
+
+    #define the fields
+    def get_len_of_name(self,object):
+        return len(object.name)
 
     #  Object-level validation   
     def validate(self, data):
